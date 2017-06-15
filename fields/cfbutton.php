f9800bd27dcf3efee736c92ccf2529474dc57456
Luka Oman <bubi-luka@users.noreply.github.com>
2017-06-16 01:04:22 +0200

    BUTTON OPEN MODAL WINDOW FOR CUSTOM FIELDS SHORTCODES
    Changes:
    - table of custom fields is no longer there
    + button that opens a modal window
    + inside are buttons for each of the custom fields
    + onClick the shortcode is inserted into the editor
    + then the modal is closed

--- fields/cfbutton.php
+++ fields/cfbutton.php
@@ -10,31 +10,40 @@ class JFormFieldCfbutton extends JFormField {
 
 	protected $type = 'cfbutton';
 
-	protected function getInput() {
-
+	protected function getInput() { 
+	
     	$html = array();
 		$html[] = '<div>';
 
-		$html[] = '<script>function insertShortcode(num) {';
-		$html[] = 'tinyMCE.activeEditor.execCommand( "mceInsertContent", false, "{field " + num + "}" );';
-		$html[] = '}</script>';
+		$html[] = '<a href="#modal-customFields" class="btn btn-warning" data-toggle="modal">Click to add a custom field</a>';
+		
+		$params = array();
+		$params['title']  = "Add a shortcode of the desired custom field";
+		$params['height'] = "400px";
+		$params['width'] = "70%";
+    
+		$windowCode = array();
+		$windowCode[] = '<script>function insertShortcode(num) {';
+		$windowCode[] = 'tinyMCE.activeEditor.execCommand("mceInsertContent", false, "{field " + num + "}");';
+		$windowCode[] = 'jQuery("#modal-customFields").modal("hide");';
+		$windowCode[] = '}</script>';
 
-		$html[] = '<table class="table table-striped">';
-		$html[] = '<thead>';
-		$html[] = '<tr>';
-		$html[] = '<th width="10%" class="nowrap center">Field ID</th>';
-		$html[] = '<th class="title">Field Name</th>';
-		$html[] = '<th width="20%" class="nowrap hidden-phone">Field Group</th>';
-		$html[] = '<th width="10%" class="nowrap hidden-phone">Field Type</th>';
-		$html[] = '</tr>';
-		$html[] = '</thead>';
+		$windowCode[] = '<table class="table">';
+		$windowCode[] = '<thead>';
+		$windowCode[] = '<tr>';
+		$windowCode[] = '<th width="10%" class="nowrap center">Field ID</th>';
+		$windowCode[] = '<th class="title">Field Name</th>';
+		$windowCode[] = '<th width="20%" class="nowrap hidden-phone">Field Group</th>';
+		$windowCode[] = '<th width="10%" class="nowrap hidden-phone">Field Type</th>';
+		$windowCode[] = '</tr>';
+		$windowCode[] = '</thead>';
 
 		// Obtain a database connection
 		$db = JFactory::getDbo();
 		
-		// Get the data for all active custom fields
+		// Get data for all active custom fields
 		$query = $db->getQuery(true)
-					->select($db->quoteName(array('id', 'label', 'type', 'fieldparams')))
+					->select($db->quoteName(array('id', 'label', 'type', 'fieldparams', 'group_id')))
 					->from($db->quoteName('#__fields'))
 					->where('state = 1');
 						
@@ -42,20 +51,42 @@ class JFormFieldCfbutton extends JFormField {
 		$db->setQuery($query);
 		
 		// Load results
-		$result = $db->loadObjectList();
+		$resultFields = $db->loadObjectList();
 		
-		foreach( $result as $row ) {					
-			$html[] = '<tr>';
-			$html[] = '<td class="center">' . $row->id . '</td>';
-			$html[] = '<td class="has-context"><a class="btn btn-small btn-block btn-success" href="javascript:void(0);" onclick="insertShortcode(' . $row->id . ');">' . $row->label . '</a></td>';
-			$html[] = '<td class="small hidden-phone"><span class="btn btn-small btn-block btn-warning" href="javascript:void(0);"></span></td>';
-			$html[] = '<td class="small hidden-phone">'  . $row->type . '</td>';
-			$html[] = '</tr>';				
-					
-			// {field ' . $row->id . '}</td><td>' . $row->label . '</td><td>' . $row->type . '</td></tr>';
+		// Get data for custom fields groups
+		$query = $db->getQuery(true)
+					->select($db->quoteName(array('id', 'title')))
+					->from($db->quoteName('#__fields_groups'));
+						
+		// Prepare the query
+		$db->setQuery($query);
+		
+		// Load results
+		$resultGroups = $db->loadObjectList();
+		
+		$groups = array();
+		
+		foreach( $resultGroups as $row ) {	
+			$groups[$row->id] = $row->title;
+		}
+		
+		foreach( $resultFields as $row ) {			
+			$windowCode[] = '<tr>';
+			$windowCode[] = '<td class="center">' . $row->id . '</td>';
+			$windowCode[] = '<td class="has-context"><a class="btn btn-small btn-block btn-success" href="javascript:void(0);" onclick="insertShortcode(' . $row->id . ');" data-dismiss="modal">' . $row->label . '</a></td>';
+			if ( $row->group_id != '0' ) {
+				$windowCode[] = '<td class="small hidden-phone"><span class="btn btn-small btn-block btn-warning" href="javascript:void(0);">' . $groups[$row->group_id] . '</span></td>';
+			}
+			else {
+				$windowCode[] = '<td class="small hidden-phone"></td>';
+			}
+			$windowCode[] = '<td class="small hidden-phone">'  . $row->type . '</td>';
+			$windowCode[] = '</tr>';
 		}		
 		
-		$html[] = '</table>';
+		$windowCode[] = '</table>';
+		
+		$html[] = JHTML::_("bootstrap.renderModal", "modal-customFields", $params, implode('', $windowCode));
 		$html[] = '</div>';
 		return implode('', $html);
 
